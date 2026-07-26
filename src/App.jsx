0495cb5975626a1fbcce5afe0af2f263b16325ed
snehasharmaa912-ops <snehasharmaa912@gmail.com>
2026-07-26 18:58:38 +0530

    Add app routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import "./App.css"
-import TopBar from "./components/TopBar";
-import NavBar from "./components/NavBar";
-import Hero from "./components/Hero";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import OurStory from "./pages/OurStory";
+import FromOurFarm from "./pages/FromOurFarm";
+import Shop from "./pages/Shop";
+import Recipes from "./pages/Recipes";
+import Blog from "./pages/Blog";
 
-import FeatureSection from "./components/FeatureSection";
-
-
-function App()
-{
-  return(
-    <div>
-        <TopBar/>
-        <NavBar/>
-        <Hero/>
-      <FeatureSection/>
-     
-    
-    </div>
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/our-story" element={<OurStory />} />
+          <Route path="/from-our-farm" element={<FromOurFarm />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/blog" element={<Blog />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
+
 export default App;
