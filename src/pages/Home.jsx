87fd7d3703ea2bf6ab5a326fd64d71a8e5c0c402
snehasharmaa912-ops <snehasharmaa912@gmail.com>
2026-07-26 18:39:05 +0530

    Add Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Hero from "../components/Hero";
+import FeatureSection from "../components/FeatureSection";
+import InstagramPopup from "../components/InstagramPopup";
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <FeatureSection />
+      <InstagramPopup />
+    </>
+  );
+}
